3eb85fe68e1c5eadc2b950f4f4d271e972b813db
jbochynska <jbochynska@egnyte.com>
2018-09-24 06:44:34 +0200

    basic structure & routing & gitignore added

---
+++ src/components/NotFound/NotFound.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class NotFound extends Component {
+  render() {
+    return (
+        <h1>Page not found</h1>
+    );
+  }
+}
+
+export default NotFound;
---
+++ src/components/WelcomePage/WelcomePage.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class WelcomePage extends Component {
+  render() {
+    return (
+        <h1>This is Welcome Page</h1>
+    );
+  }
+}
+
+export default WelcomePage;
