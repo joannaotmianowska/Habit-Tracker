d66c2c2be482f915afff91874cec5e6b21433b88
Damian <kontakt@dszymczuk.pl>
2018-12-19 19:57:09 +0100

    apply css modules for WelcomePage component

--- src/components/WelcomePage/WelcomePage.js
+++ src/components/WelcomePage/WelcomePage.js
@@ -1,24 +1,26 @@
 import React, { Component } from 'react';
 import firstImage from '../../images/undraw_High_five_u364.svg';
 
+import styles from './WelcomePage.module.scss';
+
 class WelcomePage extends Component {
   render() {
     return (
-      <div className="welcome-page-section">
-        <div className="welcome-page-row">
-            <div className="details">
+      <div className={styles.section}>
+        <div className={styles.row}>
+            <div className={styles.details}>
               <h4>Build Habits That Stick</h4>
               <p>
                 Habit Tracker helps you learn the most effective process, <br></br>
                 increase self-awareness and set priorities <br></br>
                 so achieving your goals is easier than ever.
               </p>
-              <div className="login-section">
+              <div className={styles.loginSection}>
                 <button className="fb-login">Login with Facebook</button>
                 <button className="gh-login">Login with GitHub</button>
               </div>
             </div>
-            <div className="image-section">
+            <div className={styles.imageSection}>
               <img src= { firstImage }/>
             </div>
         </div>
