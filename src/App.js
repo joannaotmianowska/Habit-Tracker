d44c96d0d43398f655b3773c10d74307b3dfe02f
Damian <kontakt@dszymczuk.pl>
2018-12-19 19:59:38 +0100

    remove unused App styles and import

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import './App.css';
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Header from './components/Header/Header';
 import Dashboard from './components/Dashboard/Dashboard';
