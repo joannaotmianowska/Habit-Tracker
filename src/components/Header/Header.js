09632d226eeab10302742797c380e9690fd600e2
Damian <kontakt@dszymczuk.pl>
2018-12-19 19:53:08 +0100

    apply css modules for Header component

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,14 +1,16 @@
 import React, { Component } from 'react';
 import logo from '../../images/HT_logo.png';
 
+import styles from './Header.module.scss';
+
 class Header extends Component {
   render() {
     return (
-        <header className="header">
-            <div className="logo-container">
-              <img src={ logo } className="logo" alt="logo" />
+        <header className={styles.header}>
+            <div className={styles.container}>
+              <img src={ logo } className={styles.image} alt="logo" />
             </div>
-            <button className="login-btn">Log in</button>
+            <button className={styles.button}>Log in</button>
         </header>
     );
   }
