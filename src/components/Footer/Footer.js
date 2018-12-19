5b455136db431b8a6935758d21da089df8215a74
Damian <kontakt@dszymczuk.pl>
2018-12-19 19:22:25 +0100

    apply css modules for Footer component

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import styles from './Footer.module.scss';
 
 class Footer extends Component {
   render() {
     return (
-      <div className="footer">
-        <span>
+      <div className={styles.footer}>
+        <span className={styles.column}>
           Contact us
           <br></br>
           office@habittracker.com
