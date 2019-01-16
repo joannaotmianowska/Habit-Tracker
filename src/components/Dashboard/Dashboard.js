75528bf9cd0d51017338cdfe21ca6c65dda3a3aa
jbochynska <jbochynska@egnyte.com>
2019-01-16 16:13:21 +0100

    login done, rest can be completed when dashboard is ready

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -4,18 +4,27 @@ import { firebaseApp } from '../../base';
 import firebase from 'firebase';
 
 class Dashboard extends Component {
-  authenticate = provider => {
-    console.log('auth', provider);
 
-    const authProvider = new firebase.auth();
+  authHandler = async (authData) => {
+    // TO DO - when dashboard is ready
+    // 1. Look up current dashboard in database
+    // 2. Claim the owner if there is no
+    // 3. Set the state of dashboard component to reflect current user
+    console.log(authData);
+  };
+
+  authenticate = provider => {
+    const authProvider = new firebase.auth[`${provider}AuthProvider`]();
     firebaseApp
       .auth()
-    console.log(authProvider)
-  }
+      .signInWithPopup(authProvider).then(this.authHandler);
+  };
 
   render() {
 
     return <LoginPage authenticate={ this.authenticate }/>
+    // TO DO - when user is logged in - show dashboard
+    // if not - show login page
     // return (
     //   <h1>This is Dashboard</h1>
     // );
